f6a549f88eceafaf8ba33b6db13e1c548b114fd7
Surya Prakash Soni <suryasonirewa@gmail.com>
2022-09-27 16:21:21 +0530

    Interface

---
+++ oops charts/InterfaceChart.java
@@ -0,0 +1,11 @@
+package OOPS;
+
+import java.io.IOException;
+
+public interface InterfaceChart {
+    void areaChart() throws IOException;
+
+    void barGraph() throws IOException;
+
+    void pieChart();
+}

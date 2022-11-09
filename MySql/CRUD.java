0eb3f9a7daaf0489a9a108f3d1f6714c96559de3
Surya Prakash Soni <suryasonirewa@gmail.com>
2022-11-09 17:58:13 +0530

    CRUD OPERATIONS.JAVA
    CRUD OPERATIONS

--- MySql/CRUD.java
+++ MySql/CRUD.java
@@ -14,6 +14,7 @@ import java.sql.*;
 import static MysqlJava.MySqlInsertion.con;
 import static MysqlJava.MySqlInsertion.logger;
 
+// CRUD OPERATIONS
 public class CRUD {
 
     // select("select * from ApiInfoBase");
